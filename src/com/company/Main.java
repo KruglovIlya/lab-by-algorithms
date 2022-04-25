04f5ea7a2be06e82fbe2ccfd82e85342f4397534
kruglov_il <kruglov_il@bk.ru>
2022-04-26 01:59:30 +0400

    2 lab

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
 
-import java.util.Locale;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static final Random rnd = new Random(System.currentTimeMillis());
 
-    public static void main(String[] args) {
+    public static void lab_1() {
         System.out.println(Encrypting.CaesarEncrypt("testing message", 1));
 
         String myString = "testing";
@@ -13,5 +15,32 @@ public class Main {
         System.out.println(Encrypting.VizhinerEncrypt(myString.toUpperCase(), "KEY"));
         System.out.println(Encrypting.VizhinerDecrypt(Encrypting.VizhinerEncrypt(myString.toUpperCase(), "KEY"), "KEY"));
 
+    }
+
+    static int getRandomNumber(int min, int max) {
+        return min + rnd.nextInt(max - min + 1);
+    }
+
+    public static void lab_2() {
+        MyQueue myQueue = new MyQueue();
+        for (int epoch = 0; epoch < 10; epoch++) {
+            long timeSum = 0;
+
+            for (int i = 0; i < 100; i++) {
+                myQueue.addPerson(getRandomNumber(180, 600));
+            }
+
+            for (int i = 0; i < 100; i++) {
+                timeSum += myQueue.pollPerson(getRandomNumber(120, 300)).getSubWithInsertAndOutTime();
+            }
+
+            System.out.println("Суммарное время 100 посетителей - " + TimeUnit.MILLISECONDS.toSeconds(timeSum) + " секунд");
+            System.out.println("В среднем посетитель стоит в очереди - " + TimeUnit.MILLISECONDS.toSeconds(timeSum/100) + " секунд");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        lab_2();
     }
 }
---
+++ src/com/company/MyQueue.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Person {
+    private final Date insertTime;
+    private Date outTime;
+
+    Person(Date initTime) {
+        this.insertTime = initTime;
+    }
+
+    Date getInsertTime() {
+        return insertTime;
+    }
+
+    void setOutTime(Date value) {
+        this.outTime = value;
+    }
+
+    long getSubWithInsertAndOutTime() {
+        return outTime.getTime() - insertTime.getTime();
+    }
+}
+
+public class MyQueue {
+    private final Date initTime = new Date();
+    private final Date currentTimeInsert = (Date) initTime.clone();
+    private final Date currentTimeOut = (Date) initTime.clone();
+
+    private final Queue<Person> queue = new LinkedList<>();
+
+    void addPerson(int countSeconds) {
+        currentTimeInsert.setTime(currentTimeInsert.getTime() + countSeconds * 1000L);
+        queue.add(new Person((Date) currentTimeInsert.clone()));
+    }
+
+    Person pollPerson(int countSeconds) {
+        Person currentPerson = queue.poll();
+        assert currentPerson != null;
+
+        if (currentTimeOut.getTime() < currentPerson.getInsertTime().getTime())
+            currentTimeOut.setTime(currentPerson.getInsertTime().getTime() + countSeconds * 1000L);
+        else
+            currentTimeOut.setTime(currentTimeOut.getTime() + countSeconds * 1000L);
+
+        currentPerson.setOutTime((Date) currentTimeOut.clone());
+
+        return currentPerson;
+    }
+}
